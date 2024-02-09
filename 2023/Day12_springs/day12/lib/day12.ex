e12cd9085657cc237e264553836a8800552ca682
thePoppe <pontus.morin@hotmail.se>
2024-02-09 14:45:24 +0100

    latest

--- 2023/Day12_springs/day12/lib/day12.ex
+++ 2023/Day12_springs/day12/lib/day12.ex
@@ -1,25 +1,27 @@
 defmodule Day12 do
+
+  # https://adventofcode.com/2023/day/12
   def main do
     {_, text} = File.read("ex1.txt");
     parsed = parse(text);
-    Enum.map(parsed, &check_row/1)
+    Enum.map(parsed, &check_row/1);
     :ok;
   end
 
-
-
   def check_row(row) do
-    [ptrn | map] = row
-    map = List.flatten(map)
+    [ptrn | map] = row;
+    map = List.flatten(map);
     IO.inspect(ptrn)
     IO.inspect(map)
   end
 
   def parse(text) do
-    list = String.split(text, "\r\n")
+    list = String.split(text, "\r\n");
     Enum.map(list, fn line ->
       [pattern, map] = String.split(line, " ");
-      [pattern | [Enum.map(String.split(map, ","), fn x -> String.to_integer(x) end)]]
-    end)
+      pattern = Enum.filter(String.split(pattern, ""),  fn x -> String.length(x)> 0 end )
+      map = Enum.map(String.split(map, ","), fn x -> String.to_integer(x); end )
+      [pattern | map]
+    end);
   end
 end
