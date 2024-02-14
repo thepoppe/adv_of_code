32c4e0d444f3d3a2dab6dc4e77a2ead7619d0191
thepoppe <pontus.morin@hotmail.se>
2024-02-14 02:42:24 +0100

    befor removing acc for sum

---
+++ 2023/Day12_springs/day12/lib/day12b.ex
@@ -0,0 +1,73 @@
+defmodule Day12b do
+  def main do
+    {_, text} = File.read("all_known.txt");
+    parsed = parse(text);
+    mem  = Mem.new();
+    {sum, mem} = Enum.reduce(parsed, {0, mem}, fn row, {sum, mem} -> check_row(row, sum, mem) end)
+    {:ok, sum};
+  end
+
+  def check_row({[], []}, acc, mem) do {1, mem} end
+  def check_row({[], _}, acc, mem) do {0, mem} end
+  def check_row({["." | rest], []}, acc, mem) do check_mem({rest, []},acc, mem) end
+  def check_row({["." | rest], map}, acc, mem) do check_mem({rest, map}, acc, mem) end
+  def check_row({["#" | _], []}, acc, mem) do {acc, mem} end
+  def check_row({["#" | rest] , [h|t]}, acc, mem) do
+    case verify_next(rest, h-1) do
+      :fail -> {acc, mem}
+      {:ok, seq, :next} -> check_mem({seq, t}, acc, mem);
+      {:ok, seq, num} -> check_mem({seq, [num|t]}, acc, mem);
+    end
+  end
+  def check_row({["?" | rest], map},acc, mem) do
+    {res, upd} = check_mem({["#" | rest], map}, acc, mem);
+    check_mem({["." | rest], map}, res, upd);
+  end
+  def check_row(_,acc, mem) do {0, mem} end
+
+  def check_mem(seq, acc, mem) do
+    case Mem.lookup(mem, seq) do
+      nil ->
+        {res, updated} = check_row(seq,acc,mem);
+        {res, Mem.store(updated, seq, res)};
+      {:found, val} ->
+        {val, mem};
+    end
+  end
+
+
+  def verify_next([], num) when num > 0 do :fail end
+  def verify_next([], _)  do {:ok, [], :next} end
+  def verify_next(["?" | rest], n) when n == 0  do {:ok, rest, :next} end
+  def verify_next(["?" | rest], n)  do {:ok, ["#" | rest], n} end
+  def verify_next(["." | rest], n) when n == 0  do {:ok, rest, :next} end
+  def verify_next(["." | _], _)  do :fail end
+  def verify_next(["#" | _], n) when n == 0  do :fail end
+  def verify_next(seq = ["#" |_], n)  do {:ok, seq, n} end
+
+  def parse(text) do
+    list = String.split(text, "\r\n");
+    Enum.map(list, fn line ->
+      [pattern, map] = String.split(line, " ");
+      pattern = Enum.filter(String.split(pattern, ""),  fn x -> String.length(x)> 0 end )
+      map = Enum.map(String.split(map, ","), fn x -> String.to_integer(x); end )
+      {pattern , map}
+    end);
+  end
+end
+
+
+defmodule Mem do
+  def new do Map.new() end
+
+  def store(mem, key, val) do
+    Map.put(mem, key, val)
+  end
+
+  def lookup(mem, key) do
+    case Map.fetch(mem, key) do
+      :error -> :nil
+      {:ok, val} -> {:found, val}
+    end
+  end
+end
