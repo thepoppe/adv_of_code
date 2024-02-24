389a4aa1ef18a8876a78a881206a579c564d1b30
thepoppe <pontus.morin@hotmail.se>
2024-02-24 03:58:10 +0100

    nope

--- 2023/Day7/day7a.ex
+++ 2023/Day7/day7a.ex
@@ -2,45 +2,58 @@ defmodule Day7a do
 
   def main do
     decks =
-      File.read!("ex.txt")
+      File.read!("real.txt")
       |> parse()
       |> Enum.map(&evaluate_decks/1)
-
-    #Enum.sort(decks, &compare_results/2)
-
+      decks = Enum.sort(decks, &compare_results/2)
+      decks = Enum.reverse(decks)
+      sum = calculate(decks, 1,0)
+    {length(decks), sum}
   end
 
-  def compare_results(deck1 = {{:type, type1}, _, _}, deck2 = {{:type, type2}, _, _}) when type1 > type2 do deck1 end
-  def compare_results(deck1 = {{:type, type1}, _, _}, deck2 = {{:type, type2}, _, _}) when type1 < type2 do deck2 end
-  def compare_results(deck1 = {_, {:score, s1}, _}, deck2 = {_, {:score, s2}, _}) when s1 <= s2 do deck1 end
-  def compare_results(deck1 = {_, {:score, s1}, _}, deck2 = {_, {:score, s2}, _}) when s1 > s2 do deck2 end
+  def calculate([],_,acc) do acc end
+  def calculate([{_,_,{:bid, bid}}|t], rank, acc) do calculate(t, rank+1, acc + (rank * bid) ) end
+
+  def compare_results({{:type, type1}, _, _}, {{:type, type2}, _, _}) when type1 != type2 do type1 > type2 end
+  def compare_results({_, {_,s1}, _}, {_, {_,s2}, _}) do s1 >= s2 end
 
 
   def evaluate_decks({deck, bid}) do
-    #IO.inspect("deck")
-    #IO.inspect(deck)
     score = Enum.map(deck, fn valor -> Enum.reduce(deck, 0, fn next, acc -> if next == valor do acc+1 else acc end end) end)
-    #IO.inspect(deck)
-    #IO.inspect(score)
     {score, sum} = case check_for_house(score, :zero) do
       :no ->
         max = Enum.max(score)
-        pairs = Enum.filter(Enum.zip(deck, score), fn {_num, score} -> score == max end)
-        sum = Enum.reduce(pairs, 0, fn {num, _}, acc -> acc + num end)
-        if max> 4 do {max + 1, sum} else {max, sum} end
+        pairs = Enum.zip(score, deck)
+        |> Enum.filter(fn {score, _valor} -> score == max end)
+        |>  remove_smaller([])
+        sum = Enum.reduce(pairs, 0, fn {_score, num}, acc -> acc + num end)
+        if max < 4 do {max, sum} else {max + 1, sum} end
       :yes ->
         {4, Enum.sum(deck)}
     end
     {{:type, score}, {:score, sum}, {:bid, bid}}
+  end
 
+  def remove_smaller([], acc) do acc end
+  def remove_smaller([first|rest], []) do remove_smaller(rest, [first]) end
+  def remove_smaller([num = {_, num1}|rest], acc = [{_, num2}| _]) do
+    if num1 < num2 do
+      acc
+    else
+      if ( num1 > num2) do
+        remove_smaller(rest, [num])
+      else
+        remove_smaller(rest, [num | acc])
+      end
+    end
   end
 
   def check_for_house([], _) do :no end
   def check_for_house([score | next], :zero) when score == 3 do check_for_house(next, :three) end
   def check_for_house([score | next], :zero) when score == 2 do check_for_house(next, :two) end
-  def check_for_house([score | next], :three) when score == 2 do :yes end
-  def check_for_house([score | next], :two) when score == 3 do :yes end
-  def check_for_house([score | next], other) do check_for_house(next, other) end
+  def check_for_house([score | _], :three) when score == 2 do :yes end
+  def check_for_house([score | _], :two) when score == 3 do :yes end
+  def check_for_house([_ | next], other) do check_for_house(next, other) end
 
 
 
@@ -52,13 +65,13 @@ defmodule Day7a do
       {cards
       |> String.split("", trim: true)
       |> Enum.reject(&(&1 == ""))
-      |> Enum.sort(:desc)
       |> Enum.map(fn str ->
         if String.match?(str, ~r/\d/) do
           String.to_integer(str)
         else convert_chars(str)
         end
-        end),
+        end)
+      |> Enum.sort(:desc),
       String.to_integer(List.to_string(bid))}
     end)
 
