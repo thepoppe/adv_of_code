45cf85f3e2be620abfadf0781218f78b7ec85106
thepoppe <pontus.morin@hotmail.se>
2024-03-09 18:59:33 +0100

    task b solved

--- 2023/Day9/day9.ex
+++ 2023/Day9/day9.ex
@@ -1,18 +1,31 @@
-defmodule Day9a do
+defmodule Day9 do
 
-  def main do
+  def taska do
     test = parse(File.read!("full.txt"))
     #test = parse(File.read!("sample.txt"))
     Enum.reduce(test, 0, fn row, acc ->
       list = expand_list(row, [], [row])
-      find_history(list, 0) + acc
+      {ahead, behind} = find_history(list, 0, 0)
+      behind + acc
     end)
   end
 
-  def find_history([], value) do value end
-  def find_history([h|t], value) do
-    find_history(t, collect_last(h, value))
+  def taskb do
+    test = parse(File.read!("full.txt"))
+    #test = parse(File.read!("sample.txt"))
+    Enum.reduce(test, 0, fn row, acc ->
+      list = expand_list(row, [], [row])
+      {ahead, behind} = find_history(list, 0, 0)
+      ahead + acc
+    end)
   end
+
+  def find_history([], ahead, behind) do {ahead ,behind} end
+  def find_history([h|t], ahead, behind) do
+    find_history(t, collect_first(h, ahead), collect_last(h, behind))
+  end
+
+  def collect_first([h|_], value) do h - value end
 
   def collect_last([], value) do value end
   def collect_last([last], value) do last+value end
