a87f2d1cebc69663b94450c44f7455e7de741e63
thepoppe <pontus.morin@hotmail.se>
2024-12-07 15:38:15 +0100

    task a solved

--- 2024/day2/task.c
+++ 2024/day2/task.c
@@ -1,9 +1,10 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define INPUTSIZE 6
-#define STR_LEN 25
+#define INPUTSIZE 1000
+#define STR_LEN 50
 #define SPACE 32
 #define LF 10
 #define CR 12
@@ -35,6 +36,7 @@ void read_input(const char *file_path, char **list) {
             str_buf[str_buf_index++] = byte;
         }
     }
+    printf("TOTAL ROWS READ: %d\n", i);
     fclose(file);
 }
 
@@ -72,13 +74,103 @@ void print_list(char **list) {
         }
     }
 }
+typedef enum { UNKNOWN, ASC, DESC } NumericalSequence;
+char *enum_toString(NumericalSequence *s) {
+    if (*s == UNKNOWN) {
+        return "UNK";
+    } else if (*s == ASC) {
+        return "ASC";
+    } else {
+        return "DESC";
+    }
+}
+
+int validate_move(int curr, int next, NumericalSequence *seq) {
+    if (curr == next || abs(curr - next) > 3) {
+        return -1;
+    }
+
+    if (*seq == UNKNOWN) {
+
+        if (curr < next) {
+            *seq = ASC;
+        } else {
+            *seq = DESC;
+        }
+        return 0;
+
+    } else if (*seq == ASC && curr < next) {
+        return 0;
+    } else if (*seq == DESC && curr > next) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+void extract_numbers(char *row, int *numbers) {
+    char str_buf[5];
+    int i = 0;
+    int j = 0;
+    while (1) {
+        if (*row == '\0') {
+            str_buf[i] = '\0';
+            numbers[j] = atoi(str_buf);
+            return;
+        }
+        if (*row != SPACE) {
+            str_buf[i++] = *row;
+        }
+        if (*row == SPACE && str_buf[0] != '\0') {
+            str_buf[i] = '\0';
+            numbers[j++] = atoi(str_buf);
+            i = 0;
+            str_buf[i] = '\0';
+        }
+        row++;
+    }
+}
+
+int compare_stringrow(char *row) {
+    int numbers[8];
+    for (int i = 0; i < 8; i++) {
+        numbers[i] = -1;
+    }
+    extract_numbers(row, numbers);
+    int i = 0;
+    int next;
+    NumericalSequence seq = UNKNOWN;
+    do {
+        int current = numbers[i];
+        int next = numbers[++i];
+
+        if (validate_move(current, next, &seq) != 0) {
+            return -1;
+        }
+
+    } while (i < 8 - 1 && numbers[i + 1] != -1);
+
+    return 0;
+}
+
+void evaluate_list(char **list) {
+    int valid_combinations = 0;
+    for (int i = 0; i < INPUTSIZE; i++) {
+        if (list[i] == NULL) {
+            break;
+        }
+        if (compare_stringrow(list[i]) == 0) {
+            valid_combinations++;
+        }
+    }
+    printf("Valid Combinations:\n%d\n", valid_combinations);
+}
 
 int main() {
-    char path[] = "example.txt";
+    char path[] = "input.txt";
     char **strings_ptr = initialize_mem();
     read_input(path, strings_ptr);
-    print_list(strings_ptr);
-
+    // print_list(strings_ptr);
+    evaluate_list(strings_ptr);
     free_mem(strings_ptr);
     strings_ptr = NULL;
 }
